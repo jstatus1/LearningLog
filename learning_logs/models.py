653fca0473ebd6c0a4189de1d7b3ab3b50a74a28
Johnson Tran <johnsontothetran1@gmail.com>
2020-01-07 17:54:48 -0600

    Adding Book Model To Learning Log

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import os
 
 # Create your models here.
 class Topic(models.Model):
@@ -22,3 +23,32 @@ class Entry(models.Model):
     def __str__(self):
         """Return a string representation of the model."""
         return f"{self.text[:50]}..."
+
+
+
+class Book(models.Model):
+    """Book model"""
+    
+    #Ratings
+    Rating_CHOICES = (
+        (1, 'Poor'),
+        (2, 'Average'),
+        (3, 'Good'),
+        (4, 'Very Good'),
+        (5, 'Excellent')
+    )
+
+    
+    title = models.CharField(max_length= 200)
+    date_added = models.DateTimeField(auto_now_add=True)
+    book_image = models.ImageField(upload_to= 'images/')
+    amazon_link = models.URLField(max_length=200, db_index=True, unique=True, blank=True)
+    summary_field = models.TextField(max_length=300)
+    rating = models.IntegerField(choices=Rating_CHOICES, default=1)
+    
+    class Meta:
+        verbose_name_plural = 'books'
+
+    def __str__(self):
+        return f"{self.title}"
+    
