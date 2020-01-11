5ed4a875830b737e82775557ba815e5800df4704
Johnson Tran <johnsontothetran1@gmail.com>
2020-01-10 18:38:39 -0600

    Set DEBUG based on environment variables.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -137,3 +137,8 @@ LOGIN_URL = 'users:login'
 
 # Heroku settings.
 django_heroku.settings(locals())
+
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG = True
+elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG = False
